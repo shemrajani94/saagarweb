8c5428c17bcc994a36683d2b3081f39a6cd916bc
shemrajani94 <saagar.hemrajani@gmail.com>
2013-10-29 17:08:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 	  color: #333; font-size: .85em; margin: 20; padding: 20;
 	  font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
  }
-   h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+   h1, h2, h3, h4,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
      h1 { font-size: 2em; }
      h2 { font-size: 1.75em; }
      h3 { font-size: 1.2em; }
+     h4 { font-size: 1.2em; }
      table { margin-top: 0.75em; }
      th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
      td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
@@ -17,10 +18,11 @@
 </head>
 <body>
 <h1>Register here!</h1>
-    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+    <p>Fill in your name, email address and company, then click <strong>Submit</strong> to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
+      Company <input type="text" name="company" id="company"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
@@ -44,14 +46,16 @@
        try {
 	 $name = $_POST['name'];
 	 $email = $_POST['email'];
+	 $company = $_POST['company'];
 	 $date = date("Y-m-d");
 	 // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date, company) 
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
         $stmt->bindValue(3, $date);
+        $stmt->bindValue(4, $company);
         $stmt->execute();
        }
        catch(Exception $e) {
@@ -69,6 +73,7 @@
        echo "<tr><th>Name</th>";
        echo "<th>Email</th>";
        echo "<th>Date</th></tr>";
+       echo "<th>Company</th></tr>";
        foreach($registrants as $registrant) {
 	 echo "<tr><td>".$registrant['name']."</td>";
 	 echo "<td>".$registrant['email']."</td>";
