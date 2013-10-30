6914aef8e3a51d63366ee1237bf09299a96c327d
shemrajani94 <saagar.hemrajani@gmail.com>
2013-10-30 00:57:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
 <h1>Register here!</h1>
     <p>Fill in your name, email address and company, then click <strong>Submit</strong> to register.</p>
-<form method="post" action="indlex.php" enctype="multipart/form-data" >
+<form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
       Company <input type="text" name="company" id="company"/></br>
