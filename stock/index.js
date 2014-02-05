e60036c55f4fa55f4dec95a69419909f5043c909
Saagar Hemrajani <saagar.hemrajani@gmail.com>
2014-02-05 19:41:52 +0000

    stock

---
+++ stock/index.js
@@ -0,0 +1,11 @@
+//var url = 'http://query.yahooapis.com/v1/public/yql';
+//var startDate = '2012-01-01';
+//var endDate = '2012-01-08';
+//var data = encodeURIComponent('select * from yahoo.finance.historicaldata where symbol in ("YHOO","AAPL","GOOG","MSFT") and startDate = "' + startDate + '" and endDate = "' + endDate + '"');
+//$.getJSON(url, 'q=' + data + "&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json", function(x){
+//    console.log(x);
+//});
+
+$.getJSON('http://finance.yahoo.com/d/quotes.jsonp?s=AAPL&f=a',function(x){
+    console.log(x);
+});
